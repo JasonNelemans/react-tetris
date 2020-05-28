8a577346a6ab3e476a2ccd58aeff247085d9a9b5
JasonNelemans <jasonnelemans@hotmail.com>
2020-05-28 12:01:11 +0200

    Styled Tetris component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { createStage } from '../gameHelpers';
+import { createStage } from "../gameHelpers";
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 //Components
-import Stage from './Stage'; 
-import Display  from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
 
 const Tetris = () => {
-
-
   return (
-     <div className='tetris'>
-        <Stage stage={createStage()}/>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
         <aside>
           <div>
-            <Display text='Score' />
-            <Display text='Rows' />
-            <Display text='Level' />
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
           </div>
           <StartButton />
         </aside>
-     </div>
-  )
-}
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
+};
 
 export default Tetris;
