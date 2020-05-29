128a4bef79421a22a5df714c07228f73cdf0fd10
JasonNelemans <jasonnelemans@hotmail.com>
2020-05-29 15:17:43 +0200

    created functions which react to user keypress

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { createStage } from '../gameHelpers';
+
 //Styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
@@ -21,8 +23,38 @@ const Tetris = () => {
 
   console.log('re-render');
 
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 })
+  }
+
+  const startGame = () => {
+    // Reset everything
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false})
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+
+  const move = ({ keyCode }) => {
+    if(!gameOver) {
+      if(keyCode === 37) { //left arrow
+        movePlayer(-1);
+      } else if(keyCode === 39) { //right arrow
+        movePlayer(1);
+      } else if (keyCode === 40) { //down arrow 
+        dropPlayer();
+      }
+    }
+  }
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -35,7 +67,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton onClick={startGame()} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
